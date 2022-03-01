171abd6e8fb738d6eb32410584d33faecdff4d91
Recursive Illusion <recursive@protonmail.com>
2022-03-01 08:25:15 -0600

    fix: removed debugMode from the main vars.
    Ideally debug would be available but I don't know exactly what the pathing would look like
    with the provider name being generated

--- terraform_providers/main.go
+++ terraform_providers/main.go
@@ -21,7 +21,7 @@ import (
 )
 
 func main() {
-	var debugMode bool
+	//var debugMode bool
 	//
 	//flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
 	//flag.Parse()
