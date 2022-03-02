74e23dae68cb78e301db35ecaa2f3e017d633f6b
Recursive <defunct.exodus@gmail.com>
2022-03-01 18:10:54 -0600

    Update main.go
    Removed dead code

--- terraform_providers/main.go
+++ terraform_providers/main.go
@@ -21,19 +21,6 @@ import (
 )
 
 func main() {
-	//var debugMode bool
-	//
-	//flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
-	//flag.Parse()
-
 	opts := &plugin.ServeOpts{ProviderFunc: Provider}
-
-	//if debugMode {
-	//	err := plugin.Debug(context.Background(), "testing", opts)
-	//	if err != nil {
-	//		log.Fatal(err.Error())
-	//	}
-	//}
 	plugin.Serve(opts)
-
 }
