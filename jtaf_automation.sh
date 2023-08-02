6ffe67d123867960b5af77ae31e8a4aa17964ee2
vinpatel24 <patelv@juniper.net>
2023-08-01 21:37:08 -0700

    adding automation file

---
+++ jtaf_automation.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# Function to check if a command is available
+command_exists() {
+  command -v "$1" >/dev/null 2>&1
+}
+
+# caputres the user's home directory
+home_dir="$PWD"
+
+# Check if Python is installed
+if ! command_exists python3; then
+  echo "Python is not installed. Please Install 'Python' before running the script"
+fi
+
+# Check if Python is installed
+if ! command_exists go; then
+  echo "Go is not installed. Please Install 'Go' before running the script."
+fi
+
+# Create config.toml file
+if [ ! -f "config.toml" ]; then
+  echo "Creating config.toml file..."
+  cat << EOF > config.toml
+yangDir = "$(pwd)/yang_files"
+providerDir = "$(pwd)/terraform_providers"
+xpathPath = "$(pwd)/xpath_example.xml"
+providerName = "vsrx"
+fileType = "both"
+EOF
+fi
+
+# Check if yang_files folder exists
+if [ -d "yang_files" ]; then
+  # Find at least one .yang file in yang_files folder
+  yang_files=$(find yang_files -name "*.yang")
+  # If YANG files are found, generate YIN and Xpath Files
+  if [ -n "$yang_files" ]; then
+    echo "Found .yang files in yang_files folder."
+    # Change directory to /cmd/processYang
+    cd $home_dir/cmd/processYang || exit 1
+    # Activate venv
+    python3 -m venv venv
+    source venv/bin/activate
+    # Check and install pyang if needed
+    if ! command_exists pyang; then
+      echo "pyang is not installed. Installing pyang..."
+      pip install pyang
+      pyang -v
+    fi
+    # Run go build command and generate YIN and Xpath Files
+    go build
+    ./processYang -config $home_dir/config.toml
+    deactivate
+    # Now Build the provider
+    cd $home_dir/cmd/processProviders
+    go build
+    ./processProviders -config $home_dir/config.toml
+    cd $home_dir/terraform_providers
+    go mod tidy -go=1.16 && go mod tidy -go=1.17
+    go build
+  else
+    echo "No .yang files found in yang_files folder. Add files and re-run script"
+  fi
+else
+  echo "yang_files folder does not exist. Create and add necessary files"
+fi
