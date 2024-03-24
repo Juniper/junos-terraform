1bf3b513c6e7ce4698e67eb8731915e146dcf164
Chris Marget <cmarget@juniper.net>
2024-03-23 23:34:10 -0400

    save key and key ID to dist dir

--- terraform_providers/scripts/release.sh
+++ terraform_providers/scripts/release.sh
@@ -2,8 +2,12 @@
 set -o pipefail
 
 DIRNAME=$(dirname -- "$(readlink -f -- "$0")")
+DIST_DIR="$DIRNAME/../dist"
 
 export PNAME=$("${DIRNAME}/provider_name.sh")
 export GPG_FINGERPRINT=$(${DIRNAME}/key_fingerprint.sh $PNAME)
 
 go run github.com/goreleaser/goreleaser release --clean --snapshot
+
+gpg --armor --export "$GPG_FINGERPRINT" > "$DIST_DIR/gpg_key.asc"
+echo "$GPG_FINGERPRINT" > "$DIST_DIR/gpg_key.id"
