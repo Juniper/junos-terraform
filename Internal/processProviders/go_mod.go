39687aa7eca51dfbe62bfb5e4ef5f3ac32b8a70b
Chris Marget <cmarget@juniper.net>
2024-03-25 19:12:02 -0400

    bump versions for smooth build

--- Internal/processProviders/go_mod.go
+++ Internal/processProviders/go_mod.go
@@ -3,10 +3,13 @@ package processProviders
 const gomodcontent = `
 module terraform-provider-junos-%+v
 
-go 1.17
+go 1.22
 
 require (
+        github.com/chrismarget/lambda-tf-registry v0.0.0-20240325230007-3e6f86f6e9c2
+        github.com/goreleaser/goreleaser v1.24.0
+        github.com/hashicorp/terraform-plugin-log v0.4.0
+        github.com/hashicorp/terraform-plugin-sdk/v2 v2.16.0
         github.com/vinpatel24/go-netconf v0.1.3
-	github.com/hashicorp/terraform-plugin-log v0.4.0
-        github.com/hashicorp/terraform-plugin-sdk/v2 v2.16
+        golang.org/x/crypto v0.18.0
 )`
