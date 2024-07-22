28a2cfc7fab6dad7deb1fb56562aede561483cf8
vishalchainani <vishal.chainani@gmail.com>
2024-07-22 18:40:16 +0530

    Create variables.tf

---
+++ Samples/dc/variables.tf
@@ -0,0 +1,12 @@
+variable "leaf_1" {
+  description = "pass leaf_1 variables"
+}
+variable "leaf_2" {
+  description = "pass leaf_2 variables"
+}
+variable "spine_1" {
+  description = "pass spine_1 variables"
+}
+variable "spine_2" {
+  description = "pass spine_2 variables"
+}
