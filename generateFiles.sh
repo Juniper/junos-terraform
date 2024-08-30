687d5e1b7a86cd5e069e500a06342f84cc099b75
Natalie <nlord@juniper.net>
2024-08-30 11:23:12 -0700

    Adding config file

--- generateFiles.sh
+++ generateFiles.sh
@@ -296,7 +296,8 @@ EOF
         go build
         ./processProviders -config $home_dir/config.toml
         cd $home_dir/terraform_providers
-        go mod tidy -go=1.16 && go mod tidy -go=1.17
+        # go mod tidy -go=1.16 && go mod tidy -go=1.17
+        go mod tidy -go=1.23
         go build
     else
         echo "No XML xpath file found. Try renaming the xpath file to include 'xpath' in the name."
