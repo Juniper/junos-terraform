0dc507fd660cb3793de80bd46d269aa467453d16
vishalchainani <vishal.chainani@gmail.com>
2024-11-07 13:05:29 +0530

    Create main.tf

---
+++ testing_folder/main.tf
@@ -0,0 +1,27 @@
+ terraform {
+  required_providers {
+    junos-vsrx = {
+      source = "juniper/providers/junos-vsrx"
+      version = "21.31.108"
+    }
+  }
+}
+resource "junos-vsrx_Interfaces" "vsrx_2" {
+  resource_name = "example_resource"
+  name          = "ge-0/0/0"
+  description   = "Main Ethernet"
+  mtu           = 9192
+  vlan_tagging = true
+  units = [
+    {
+      name        = "0"
+      description = "unit_desc"
+      vlan_id = 10
+    },
+    {
+      name        = "1"
+      description = "unit_desc"
+      vlan_id = 20  
+    }
+  ]
+}
