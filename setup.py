fe508afae6d6e1c8f879becb8a415ce3b11cc9f1
aburston <aburston@juniper.net>
2025-06-30 18:10:18 +0100

    mreged ab changes

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ setup(
     url='https://github.com/aburston/junos-terraform',
     author="Juniper Networks",
     description="Junos Terraform Framework",
-    #packages=find_packages(),
-    packages=["junosterraform", "jtaf_pyang_plugin"],
-    scripts=["junosterraform/jtaf-provider", "jtaf_pyang_plugin/jtaf-pyang-plugindir", "junosterraform/jtaf-junos-xml2tf"],
+    packages=["junosterraform", "jtaf_pyang_plugin", "terraform_provider"],
+    scripts=["junosterraform/jtaf-provider", "jtaf_pyang_plugin/jtaf-pyang-plugindir", "junosterraform/jtaf-xml2tf"],
     package_data = {
-        "junosterraform": [ "terraform_provider/*", "terraform_provider/netconf/*", "templates/*" ]
+        "terraform_provider": [ "*", "netconf/*" ],
+        "junosterraform": [ "templates/*" ]
     },
     install_requires=[
         'pytest',
