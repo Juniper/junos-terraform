f4e5616236e13b1a88a1a43e237bdf412b1883f2
vishalchainani <vishal.chainani@gmail.com>
2025-09-11 17:45:16 +0530

    Update convert.sh

--- examples/providers/convert.sh
+++ examples/providers/convert.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-jtaf-xml2tf -x examples/evpn-vxlan-dc/dc1/*{spine,leaf}*.xml -j examples/providers/terraform-provider-junos-qfx-evpn-vxlan/trimmed_schema.json -t qfx -d examples/terraform_files -u jcluser -p 'Junipergit -help'
+jtaf-xml2tf -x ../evpn-vxlan-dc/dc1/*{spine,leaf}*.xml -j terraform-provider-junos-qfx-evpn-vxlan/trimmed_schema.json -t qfx -d ../terraform_files -u jcluser -p 'Juniper!1'
