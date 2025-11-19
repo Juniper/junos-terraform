fd9b981d470afd5b49900ef0fbeada87784109ba
Vasily Pomuran <vpomuran@juniper.net>
2025-11-19 14:16:11 +0100

    Finished first jtaf-ansible verison that generates both main.j2 template and yaml file

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from setuptools.command.install import install
 
 setup(
     name='junosterraform',
-    version='1.0.0',
+    version='1.0.1',
     url='https://github.com/aburston/junos-terraform',
     author="Juniper Networks",
     description="Junos Terraform Framework",
     packages=["junosterraform", "jtaf_pyang_plugin", "terraform_provider"],
-    scripts=["junosterraform/jtaf-provider", "jtaf_pyang_plugin/jtaf-pyang-plugindir", "junosterraform/jtaf-xml2tf", "junosterraform/jtaf-yang2go"],
+    scripts=["junosterraform/jtaf-provider","junosterraform/jtaf-ansible", "jtaf_pyang_plugin/jtaf-pyang-plugindir", "junosterraform/jtaf-xml2tf", "junosterraform/jtaf-yang2go",
+             "junosterraform/jtaf-yang2ansible"],
     package_data = {
         "terraform_provider": [
             "*",
