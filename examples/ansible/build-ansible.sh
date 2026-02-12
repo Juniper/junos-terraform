37223fce04f08768298dff7400c237287915b13b
vishalchainani <vishal.chainani@gmail.com>
2026-02-12 16:40:30 +0530

    Update path for junos-es in build-ansible.sh
    corrected the path of YANG modules for SRX

--- examples/ansible/build-ansible.sh
+++ examples/ansible/build-ansible.sh
@@ -7,4 +7,4 @@ set -e
 jtaf-yang2ansible -p ../../../yang/18.2/18.2R3/common ../../../yang/18.2/18.2R3/junos-qfx/conf/*.yang -x ../evpn-vxlan-dc/dc1/dc1-*leaf* ../evpn-vxlan-dc/dc1/dc1-*spine* ../evpn-vxlan-dc/dc2/dc2-*spine* -t vqfx-ansible-role
 
 
-jtaf-yang2ansible -p ../../../yang/18.2/18.2R3/common ../../../yang/18.2/18.2R3/junos-qfx/conf/*.yang -x ../evpn-vxlan-dc/dc1/dc1-*firewall* ../evpn-vxlan-dc/dc2/dc2-*firewall* -t srx-ansible-role
+jtaf-yang2ansible -p ../../../yang/18.2/18.2R3/common ../../../yang/18.2/18.2R3/junos-es/conf/*.yang -x ../evpn-vxlan-dc/dc1/dc1-*firewall* ../evpn-vxlan-dc/dc2/dc2-*firewall* -t srx-ansible-role
