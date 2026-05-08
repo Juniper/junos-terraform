07367be4ff87ac8065a7f1a937d37cad0ddd2c43
Vishal Chainani <vishalc@juniper.net>
2026-05-08 10:54:50 +0530

    added dc2 nodes in convert-ansible.sh script

--- examples/ansible/convert-ansible.sh
+++ examples/ansible/convert-ansible.sh
@@ -10,8 +10,8 @@ if [[ "$RESET_PAYLOAD_CACHE" == "1" ]]; then
 	rm -f ansible_files/group_vars/all.yaml
 fi
 
-jtaf-xml2yaml -x ../evpn-vxlan-dc/dc1/dc1-*leaf* ../evpn-vxlan-dc/dc1/dc1-*spine* -j ansible-provider-junos-vqfx-ansible-role/trimmed_schema.json -d ansible_files --grouping-hosts-file switches_grouping_hosts
+jtaf-xml2yaml -x ../evpn-vxlan-dc/dc1/dc1-*leaf* ../evpn-vxlan-dc/dc1/dc1-*spine* ../evpn-vxlan-dc/dc2/dc2-*spine* -j ansible-provider-junos-vqfx-ansible-role/trimmed_schema.json -d ansible_files --grouping-hosts-file switches_grouping_hosts
 
-jtaf-xml2yaml -x ../evpn-vxlan-dc/dc1/dc1-*firewall* -j ansible-provider-junos-srx-ansible-role/trimmed_schema.json -d ansible_files --grouping-hosts-file firewall_grouping_hosts
+jtaf-xml2yaml -x ../evpn-vxlan-dc/dc1/dc1-*firewall* ../evpn-vxlan-dc/dc2/dc2-*firewall* -j ansible-provider-junos-srx-ansible-role/trimmed_schema.json -d ansible_files --grouping-hosts-file firewall_grouping_hosts
 
 
