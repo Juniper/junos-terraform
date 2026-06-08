abfb0d805dd5259240559ec2bcef4eda710e8215
Prachi <prachi@hpe.com>
2026-06-08 10:20:44 +0530

    Remove generated example artifacts from fork

--- examples/terraform_files/providers.tf
+++ examples/terraform_files/providers.tf
@@ -11,105 +11,105 @@ terraform {
 
 
 provider "junos-vqfx-evpn-vxlan" {
-    host     = var.junos_vqfx_endpoints["dc1_spine1"].host
-    port     = var.junos_vqfx_endpoints["dc1_spine1"].port
-    username = var.junos_username
-    password = var.junos_password
+    host     = "dc1-spine1"
+    port     = 22
+    username = "jcluser"
+    password = "Juniper!1"
     alias    = "dc1_spine1"
 }
 
 provider "junos-vqfx-evpn-vxlan" {
-    host     = var.junos_vqfx_endpoints["dc1_spine2"].host
-    port     = var.junos_vqfx_endpoints["dc1_spine2"].port
-    username = var.junos_username
-    password = var.junos_password
+    host     = "dc1-spine2"
+    port     = 22
+    username = "jcluser"
+    password = "Juniper!1"
     alias    = "dc1_spine2"
 }
 
 provider "junos-vqfx-evpn-vxlan" {
-    host     = var.junos_vqfx_endpoints["dc1_borderleaf1"].host
-    port     = var.junos_vqfx_endpoints["dc1_borderleaf1"].port
-    username = var.junos_username
-    password = var.junos_password
+    host     = "dc1-borderleaf1"
+    port     = 22
+    username = "jcluser"
+    password = "Juniper!1"
     alias    = "dc1_borderleaf1"
 }
 
 provider "junos-vqfx-evpn-vxlan" {
-    host     = var.junos_vqfx_endpoints["dc1_borderleaf2"].host
-    port     = var.junos_vqfx_endpoints["dc1_borderleaf2"].port
-    username = var.junos_username
-    password = var.junos_password
+    host     = "dc1-borderleaf2"
+    port     = 22
+    username = "jcluser"
+    password = "Juniper!1"
     alias    = "dc1_borderleaf2"
 }
 
 provider "junos-vqfx-evpn-vxlan" {
-    host     = var.junos_vqfx_endpoints["dc1_leaf1"].host
-    port     = var.junos_vqfx_endpoints["dc1_leaf1"].port
-    username = var.junos_username
-    password = var.junos_password
+    host     = "dc1-leaf1"
+    port     = 22
+    username = "jcluser"
+    password = "Juniper!1"
     alias    = "dc1_leaf1"
 }
 
 provider "junos-vqfx-evpn-vxlan" {
-    host     = var.junos_vqfx_endpoints["dc1_leaf2"].host
-    port     = var.junos_vqfx_endpoints["dc1_leaf2"].port
-    username = var.junos_username
-    password = var.junos_password
+    host     = "dc1-leaf2"
+    port     = 22
+    username = "jcluser"
+    password = "Juniper!1"
     alias    = "dc1_leaf2"
 }
 
 provider "junos-vqfx-evpn-vxlan" {
-    host     = var.junos_vqfx_endpoints["dc1_leaf3"].host
-    port     = var.junos_vqfx_endpoints["dc1_leaf3"].port
-    username = var.junos_username
-    password = var.junos_password
+    host     = "dc1-leaf3"
+    port     = 22
+    username = "jcluser"
+    password = "Juniper!1"
     alias    = "dc1_leaf3"
 }
 
 provider "junos-vqfx-evpn-vxlan" {
-    host     = var.junos_vqfx_endpoints["dc2_spine1"].host
-    port     = var.junos_vqfx_endpoints["dc2_spine1"].port
-    username = var.junos_username
-    password = var.junos_password
+    host     = "dc2-spine1"
+    port     = 22
+    username = "jcluser"
+    password = "Juniper!1"
     alias    = "dc2_spine1"
 }
 
 provider "junos-vqfx-evpn-vxlan" {
-    host     = var.junos_vqfx_endpoints["dc2_spine2"].host
-    port     = var.junos_vqfx_endpoints["dc2_spine2"].port
-    username = var.junos_username
-    password = var.junos_password
+    host     = "dc2-spine2"
+    port     = 22
+    username = "jcluser"
+    password = "Juniper!1"
     alias    = "dc2_spine2"
 }
 
 provider "junos-vsrx-evpn-vxlan" {
-    host     = var.junos_vsrx_endpoints["dc1_firewall1"].host
-    port     = var.junos_vsrx_endpoints["dc1_firewall1"].port
-    username = var.junos_username
-    password = var.junos_password
+    host     = "dc1-firewall1"
+    port     = 22
+    username = "jcluser"
+    password = "Juniper!1"
     alias    = "dc1_firewall1"
 }
 
 provider "junos-vsrx-evpn-vxlan" {
-    host     = var.junos_vsrx_endpoints["dc1_firewall2"].host
-    port     = var.junos_vsrx_endpoints["dc1_firewall2"].port
-    username = var.junos_username
-    password = var.junos_password
+    host     = "dc1-firewall2"
+    port     = 22
+    username = "jcluser"
+    password = "Juniper!1"
     alias    = "dc1_firewall2"
 }
 
 provider "junos-vsrx-evpn-vxlan" {
-    host     = var.junos_vsrx_endpoints["dc2_firewall1"].host
-    port     = var.junos_vsrx_endpoints["dc2_firewall1"].port
-    username = var.junos_username
-    password = var.junos_password
+    host     = "dc2-firewall1"
+    port     = 22
+    username = "jcluser"
+    password = "Juniper!1"
     alias    = "dc2_firewall1"
 }
 
 provider "junos-vsrx-evpn-vxlan" {
-    host     = var.junos_vsrx_endpoints["dc2_firewall2"].host
-    port     = var.junos_vsrx_endpoints["dc2_firewall2"].port
-    username = var.junos_username
-    password = var.junos_password
+    host     = "dc2-firewall2"
+    port     = 22
+    username = "jcluser"
+    password = "Juniper!1"
     alias    = "dc2_firewall2"
 }
